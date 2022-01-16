352731d2e2031e8b70174a2651b179013f3ebce9
laxman-taur9 <laxmantaur9@gmail.com>
2022-01-16 18:00:42 +0530

    Modified: Calculate part time Emp Wage

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,16 +1,18 @@
 package com.bridgelabz;
 
 public class EmpWage {
-	int IS_FULL_TIME = 1;
+	int IS_FULL_TIME = 2;
+    int IS_PART_TIME = 1;
 	int EMP_RATE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
 	
 	public void emp() {
 	
-double empCheck = Math.floor(Math.random()*10)%2;
- 
-	 if(empCheck==IS_FULL_TIME)
+double empCheck = Math.floor(Math.random()*10)%3;
+ if(empCheck == IS_PART_TIME)
+	 empHrs = 4;
+ else if(empCheck==IS_FULL_TIME)
 		 empHrs = 8;
 	 else
 		 empHrs = 0;
