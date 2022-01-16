f0c61bb0d933aeb52a2810bedf133c9599328920
laxman-taur9 <laxmantaur9@gmail.com>
2022-01-16 18:19:38 +0530

    Modified: Calculating wages for month

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,11 +4,13 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS = 2;
 	int empHrs = 0;
 	int empWage = 0;
+	int totalEmpWage = 0;
 	
 	public void emp() {
-	
+	for(int day=0; day<NUM_OF_WORKING_DAYS;day++) {
 int empCheck =(int) Math.floor(Math.random()*10)%3;
  switch(empCheck) {
  case IS_PART_TIME:
@@ -22,7 +24,10 @@ int empCheck =(int) Math.floor(Math.random()*10)%3;
  }	
 		
 	 empWage = empHrs * EMP_RATE_PER_HOUR;
+	 totalEmpWage +=empWage;
 	 System.out.println("Emp Wage: "+empWage);
+	}
+	System.out.println("Total Emp Wages:"+totalEmpWage);
 	 }
 	public static void main(String[] args) {
 		EmpWage em= new EmpWage();
