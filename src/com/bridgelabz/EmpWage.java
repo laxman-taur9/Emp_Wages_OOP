7442fed394db278dc5e4e6e26fff411d2b457f3c
laxman-taur9 <laxmantaur9@gmail.com>
2022-01-16 18:32:54 +0530

    Modified: total calculating working days

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,11 +4,15 @@ public class EmpWage {
 	public static final int IS_FULL_TIME = 2;
     public static final int IS_PART_TIME = 1;
 	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUM_OF_WORKING_DAYS=2;
+	public static final int MAX_HRS_IN_MONTH=10;
 	int empHrs = 0;
-	int empWage = 0;
-	
+	int totalEmpHrs = 0;
+	int totalWorkingDays=0;
 	public void emp() {
-	
+	while (totalEmpHrs<=MAX_HRS_IN_MONTH && totalWorkingDays<NUM_OF_WORKING_DAYS) {
+		totalWorkingDays++;
+
 int empCheck =(int) Math.floor(Math.random()*10)%3;
  switch(empCheck) {
  case IS_PART_TIME:
@@ -21,8 +25,11 @@ int empCheck =(int) Math.floor(Math.random()*10)%3;
 	 empHrs = 0;
  }	
 		
-	 empWage = empHrs * EMP_RATE_PER_HOUR;
-	 System.out.println("Emp Wage: "+empWage);
+	 totalEmpHrs = empHrs * EMP_RATE_PER_HOUR;
+	 System.out.println("Day#: "+totalWorkingDays+"Emp Hrs:"+empHrs);
+	}
+	int totalEmpWage = totalEmpHrs* EMP_RATE_PER_HOUR;
+	System.out.println("Total Emp Wage"+totalEmpWage);
 	 }
 	public static void main(String[] args) {
 		EmpWage em= new EmpWage();
