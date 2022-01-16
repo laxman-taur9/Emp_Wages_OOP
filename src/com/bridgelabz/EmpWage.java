4ed22f0771657eeefc5bb481e02d7a6fafda1014
laxman-taur9 <laxmantaur9@gmail.com>
2022-01-16 18:10:03 +0530

    Modified: calculate Emp Wage Using Switch

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,21 +1,26 @@
 package com.bridgelabz;
 
 public class EmpWage {
-	int IS_FULL_TIME = 2;
-    int IS_PART_TIME = 1;
-	int EMP_RATE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 2;
+    public static final int IS_PART_TIME = 1;
+	public static final int EMP_RATE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
 	
 	public void emp() {
 	
-double empCheck = Math.floor(Math.random()*10)%3;
- if(empCheck == IS_PART_TIME)
+int empCheck =(int) Math.floor(Math.random()*10)%3;
+ switch(empCheck) {
+ case IS_PART_TIME:
 	 empHrs = 4;
- else if(empCheck==IS_FULL_TIME)
-		 empHrs = 8;
-	 else
-		 empHrs = 0;
+	 break;
+ case IS_FULL_TIME:	 
+ empHrs = 8;
+ break;
+ default:
+	 empHrs = 0;
+ }	
+		
 	 empWage = empHrs * EMP_RATE_PER_HOUR;
 	 System.out.println("Emp Wage: "+empWage);
 	 }
